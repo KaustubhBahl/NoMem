7bf1b3ba164fad192e853e75f38a3a71ccf0dd8f
Kaustubh Bahl <kbahl098@gmail.com>
2023-04-04 01:07:17 +0530

    add create password and view password functions

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:nomem/pages/home.dart';
+import 'package:nomem/pages/accountslist.dart';
+import 'package:nomem/pages/create_account.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'NoMem',
+        routes: {
+          '/': (context) => const Home(),
+          '/addAccount': (context) => const AddAccount(),
+          '/accountsList': (context) => const AccountsList(),
+        });
+  }
+}
