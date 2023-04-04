4d566226c255d5c0038a35a281efbf2391b67726
Gigil James <70816318+gigiljames@users.noreply.github.com>
2023-04-04 23:28:11 +0530

    account search

--- lib/pages/accountslist.dart
+++ lib/pages/accountslist.dart
@@ -26,6 +26,127 @@ class Account {
   });
 }
 
+class CustomSearchDelegate extends SearchDelegate {
+// Demo list to show querying
+  List<Account> searchTerms = [
+    Account.withIcon(domain: 'Facebook', user: 'billy@gmail.com', passwordLength: 12, versionNumber: 1, icon1: 'https://img.icons8.com/color/256/facebook-new.png'),
+    Account.withIcon(domain: 'Google', user: 'billy@gmail.com', passwordLength: 16, versionNumber: 2, icon1: 'https://img.icons8.com/color/256/google-logo.png'),
+    Account.withIcon(domain: 'SBI', user: 'billyjoel_951949', passwordLength: 12, versionNumber: 34, icon1: 'https://img.icons8.com/color/256/test-account.png'),
+  ];
+
+// first overwrite to
+// clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      ),
+    ];
+  }
+
+// second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+// third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    List<Account> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.domain.toLowerCase().contains(query.toLowerCase()) || fruit.user.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result.domain),
+        );
+      },
+    );
+  }
+
+// last overwrite to show the
+// querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<Account> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.domain.toLowerCase().contains(query.toLowerCase()) || fruit.user.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        // return ListTile(
+        //   title: Text(result.domain),
+        // );
+        return Container(
+          margin: const EdgeInsets.fromLTRB(12.0, 5.0, 12.0, 0),
+          child: Card(
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  color: Color.fromRGBO(103, 80, 164, 0.05),
+                ),
+                borderRadius: BorderRadius.circular(15.0), //<-- SEE HERE
+              ),
+              // margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+              child: TextButton(
+                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => AccountDetailsPage(domain: result.domain, username: result.user, passwordLength: result.passwordLength, versionNumber: result.versionNumber)));},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromRGBO(103, 80, 164, 0.05)),
+                  //   // shadowColor: MaterialStateProperty.all(
+                  //     const Color.fromRGBO(255, 255, 255, 1)),
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      ListTile(leading: Image(
+                        image: NetworkImage(result.icon1),
+                      ),
+                          title: Text(
+                              result.domain,
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black,
+                              )
+                          ),
+                          // const SizedBox(height: 8.0),
+                          subtitle: Text(
+                              result.user,
+                              style: const TextStyle(
+                                  fontSize: 12.0,
+                                  color: Colors.black
+                              )
+                          )
+                      )]
+                ),
+              )
+          ),
+        );
+      },
+    );
+  }
+}
+
+
 class AccountsList extends StatefulWidget{
   const AccountsList({super.key});
 
@@ -48,13 +169,6 @@ class _AccountsListState extends State<AccountsList> {
   Widget accountTemplate(account, String i){
     return Container(
       margin: const EdgeInsets.fromLTRB(12.0, 5.0, 12.0, 0),
-      // decoration: const BoxDecoration(
-      //   boxShadow: [
-      //     BoxShadow(
-      //       blurRadius: 10.0,
-      //     ),
-      //   ],
-      // ),
       child: Card(
         elevation: 3,
           shape: RoundedRectangleBorder(
@@ -110,11 +224,17 @@ class _AccountsListState extends State<AccountsList> {
                       ),
         actions: <Widget>[
           IconButton(
-            icon : Icon(Icons.search,color: Colors.black,),
-            onPressed: (){},
+            icon : const Icon(Icons.search,color: Colors.black,),
+            onPressed: (){
+              showSearch(
+                  context: context,
+                  // delegate to customize the search bar
+                  delegate: CustomSearchDelegate()
+              );
+            },
           ),
           IconButton(
-            icon : Icon(Icons.sort_by_alpha,color: Colors.black,),
+            icon : const Icon(Icons.sort_by_alpha,color: Colors.black,),
             onPressed: (){},
           ),
         ],
