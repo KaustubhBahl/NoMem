c7afe7ef5c5fff1227c3774398d1496eddbbec44
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-04-07 22:10:36 +0530

    Update passwordGen.dart

--- lib/passwordGen.dart
+++ lib/passwordGen.dart
@@ -1,6 +1,91 @@
 import 'package:crypto/crypto.dart';
 import 'dart:convert';
 
+String modify(int bv,int index,int sum,int pos1,int pos2,int pos3,int pos4)
+{
+  int x=0;
+  int fv=((bv%77)*(index%77)*(sum%77))%77;
+  if(index==pos1)
+  {
+    x=fv%15+'!'.codeUnitAt(0);
+  }
+  else if(index==pos2)
+  {
+    x=fv%26+'A'.codeUnitAt(0);
+  }
+  else if(index==pos3)
+  {
+    x=fv%26+'a'.codeUnitAt(0);
+  }
+  else if(index==pos4)
+  {
+    x=fv%10+'0'.codeUnitAt(0);
+  }
+  else
+  {
+    x=usual_modify(bv,index,sum);
+  }
+  return String.fromCharCode(x);
+  
+}  
+int usual_modify(int bv,int index,int sum)
+{
+  int x;
+  int fv=((bv%77)*(index%77)*(sum%77))%77;
+  if(fv>=0 && fv<15)
+  {
+    x=fv%15+'!'.codeUnitAt(0);
+  }
+  else
+  {
+    if(fv>=15 && fv<41)
+    {
+      x=fv%26+'A'.codeUnitAt(0);
+    }
+    else
+    {
+      if(fv>=41 && fv<67)
+      {
+        x=fv%26+'a'.codeUnitAt(0);
+      }
+      else
+      {
+        x=fv%10+'0'.codeUnitAt(0);
+      }
+    } 
+  }
+  return x;
+}
+String passgen(String str,int len)
+{
+  int j=0;
+  int sum=0;
+  while(j<str.length)
+  {
+    sum=sum+str[j].codeUnitAt(0);
+    j++;
+  }
+  int pos1,pos2,pos3,pos4;
+  pos1=(len~/4)-1;
+  pos2=2*(len~/4)-1;
+  pos3=3*(len~/4)-1;
+  pos4=4*(len~/4)-1;
+  print('$pos1  $pos2  $pos3  $pos4\n');
+  String password='';
+  j=0;
+  while(j<len)
+  {
+    password=password+modify(str[j].codeUnitAt(0),j,sum,pos1,pos2,pos3,pos4);
+    j++;
+  }
+  return password;
+}
+
+
+
+
+
+
 class PasswordGen {
   final String domain, username, length, version, userKey;
 
@@ -23,11 +108,7 @@ class PasswordGen {
       sum += digestHex[i].codeUnitAt(0);
       i++;
     }
-    final password = digestHex.substring(0, len - 4) +
-        String.fromCharCode(sum % 15 + '!'.codeUnitAt(0)) +
-        String.fromCharCode(sum % 26 + 'A'.codeUnitAt(0)) +
-        String.fromCharCode(sum % 26 + 'a'.codeUnitAt(0)) +
-        String.fromCharCode(sum % 10 + '0'.codeUnitAt(0));
+    final password = passgen(digestHex,len);
     return password;
   }
 }
